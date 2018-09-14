9c005c2ec694748be3370bd976c9c59203298570
nguyenhuuthanh3199 <thanhnh99.amc@gmail.com>
2018-09-14 16:12:38 +0700

    nộp bài
    nộp bài

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -2,15 +2,57 @@ package week1;
 
 public class Student {
 
-    // TODO: khai báo các thuộc tính cho Student
+    // TODO: khai bĂ¡o cĂ¡c thuá»™c tĂ­nh cho Student
+    private String name;//ten sinh vien
+    private String id;//ma sinh vien
+    private String group;//lop hoc
+    private String email;//dia chi email
 
-    // TODO: khai báo các phương thức getter, setter cho Student
+    // TODO: khai bĂ¡o cĂ¡c phÆ°Æ¡ng thá»©c getter, setter cho Student
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
 
     /**
      * Constructor 1
      */
+    
     Student() {
         // TODO:
+        this.name="Student";
+        this.id="000";
+        this.group="INT22041";
+        this.email="uet@vnu.edu.vn";
     }
 
     /**
@@ -21,6 +63,9 @@ public class Student {
      */
     Student(String n, String sid, String em) {
         // TODO:
+        this.name=n;
+        this.id=sid;
+        this.email=em;
     }
 
     /**
@@ -29,10 +74,16 @@ public class Student {
      */
     Student(Student s) {
         // TODO:
+        this.name=s.name;
+        this.email=s.email;
+        this.id=s.id;
+        this.group=s.group;
     }
 
     String getInfo() {
         // TODO:
-        return null; // xóa dòng này sau khi cài đặt
+        String a="Name: "+this.name+"\nID: "+this.id+"\nGroup: "+this.group+"\nEmail: "+this.email;
+        
+        return a; 
     }
 }
--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -1,12 +1,16 @@
 package week1;
 
+import java.util.Scanner;
+
 public class StudentManagement {
 
-    // TODO: khai báo thuộc tính students là array chứa các đối tượng thuộc lớp Student (max. 100)
+    // TODO: khai bĂ¡o thuá»™c tĂ­nh students lĂ  array chá»©a cĂ¡c Ä‘á»‘i tÆ°á»£ng thuá»™c lá»›p Student (max. 100)
+    public Student [] Student_arr=new Student[100];
 
     public boolean sameGroup(Student s1, Student s2) {
         // TODO:
-        return false; // xóa dòng này sau khi cài đặt
+        
+        return s1.getGroup().equals(s2.getGroup()); 
     }
 
     void studentsByGroup() {
@@ -15,9 +19,45 @@ public class StudentManagement {
 
     void removeStudent(String id) {
         // TODO:
+        int i=0;
+        for(Student a:Student_arr)
+        {
+            i++;
+            if(a.getId().equals(id))
+            {
+                System.out.println("Xoa sinh vien thu" +i);
+            }
+        }
     }
 
     public static void main(String[] args) {
         // TODO:
+//        Student thanh=new Student();
+//        thanh.setGroup("INT2204-5");
+//        Student me=new Student();
+//        me.setName("Nguyen Huu Thanh");
+//        me.setId("17021014");
+//        me.setGroup("INT2204-5");
+//        me.setEmail("thanhnh99.amc@gmail.com");
+//        System.out.println("Cung lop");
+//        System.out.println(me.getName());
+//        System.out.println(me.getInfo());
+          StudentManagement St_list=new StudentManagement();
+          for(int i=0;i<5;i++)
+          {
+              St_list.Student_arr[i]=new Student();
+              St_list.Student_arr[i].setId(new Scanner(System.in).nextLine());
+          }
+//          if(St_list.sameGroup(St_list.Student_arr[0], St_list.Student_arr[1]))
+//          {
+//              System.out.println("Hai hoc sinh cung lop");
+//          }
+//          else 
+//          {
+//              System.out.println("Hai hoc sinh khong cung lop");
+//          };
+          St_list.removeStudent("int22024");
+          
+        
     }
 }
