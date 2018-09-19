45f350ea76e3e336ac44fec2c533540b942ef81c
Nguyenhuuthanh <thanhnh99.amc@github.com>
2018-09-20 00:41:39 +0700

    week1 update

--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -1,7 +1,5 @@
 package week1;
 
-import java.util.Scanner;
-
 public class StudentManagement {
     // TODO: khai báo thuộc tính students là array chứa các đối tượng thuộc lớp Student (max. 100)
     public Student [] Student_arr=new Student[100];
@@ -17,11 +15,45 @@ public class StudentManagement {
         
         return s1.getGroup().equals(s2.getGroup()); 
     }
-
+    void sortGroup()
+    {
+        for(int i=0;i<StudentSum;i++)
+        {
+            for(int j=i+1;j<StudentSum;j++)
+            {
+                if(0<Student_arr[i].getGroup().compareTo(Student_arr[j].getGroup()))
+                {
+                    Student x;
+                    x=Student_arr[i];
+                    Student_arr[i]=Student_arr[j];
+                    Student_arr[j]=x;
+                }
+            }
+        }
+    }
     void studentsByGroup() {
         // TODO:
         //sap xep sinh vien theo lop
-        
+        sortGroup();
+        //
+        int StuSameGroup;
+        for (int i=0;i<StudentSum;i+=StuSameGroup)
+        {
+            StuSameGroup=0;
+            System.out.println("Group: "+Student_arr[i].getId());
+            for(int j=i;j<StudentSum;j++)
+            {
+                if(Student_arr[i].getGroup().equals(Student_arr[j].getGroup()))
+                {
+                    System.out.println(Student_arr[j].getName());
+                    StuSameGroup++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
         
     }
     void Student_Management_out()
@@ -36,7 +68,7 @@ public class StudentManagement {
         // TODO:
         for(int i=0;i<StudentSum;i++)
         {
-            if(Student_arr[i].getId()==id)
+            if(Student_arr[i].getId().equals(id))
             {
                 for(int j=i;j<StudentSum;j++)
                 {
@@ -53,8 +85,9 @@ public class StudentManagement {
         // TODO:
         StudentManagement Student_Management=new StudentManagement();
         //cau 6
+        System.out.println("--------Câu 6--------");
         //6a
-       Student thanh=new Student();
+        Student thanh=new Student();
        //6b
         thanh.setGroup("INT2204-5");
         Student me=new Student();
@@ -63,17 +96,24 @@ public class StudentManagement {
         me.setGroup("INT2204-5");
         me.setEmail("thanhnh99.amc@gmail.com");
         //6c
+        System.out.println("--------test getname()--------");
         System.out.println(me.getName());
         //6d
+        System.out.println("--------test getInfo()--------");
         me.getInfo();
         //cau 8
+        System.out.println("-------Câu 8--------");
+        System.out.println("Test constructor Student()");
         Student a=new Student();
         a.getInfo();
+        System.out.println("Test constructor Student(String n, String id, String e");
         Student b=new Student("Nguyen Huu Thanh","17021014","thanhnh99.amc@gmail.com");
         b.getInfo();
+        System.out.println("Test contructor Student(Student s)");
         Student c=new Student(b);
         c.getInfo();
         //cau 10
+        System.out.println("--------Câu 10--------\nTest sameGroup");  
         Student d=new Student(),e=new Student(),f=new Student();
         f.setGroup("INT22042");
         System.out.println("Neu cung lop in ra true, khac lop in ra false.");
@@ -91,21 +131,20 @@ public class StudentManagement {
         Student_Management.removeStudent("17021014");
         Student_Management.Student_Management_out();
         
-//        
-//          StudentManagement St_list=new StudentManagement();
-//          for(int i=0;i<5;i++)
-//          {
-//              St_list.Student_arr[i]=new Student();
-//              St_list.Student_arr[i].setId(new Scanner(System.in).nextLine());
-//          }
-//          if(St_list.sameGroup(St_list.Student_arr[0], St_list.Student_arr[1]))
-//          {
-//              System.out.println("Hai hoc sinh cung lop");
-//          }
-//          else 
-//          {
-//              System.out.println("Hai hoc sinh khong cung lop");
-//          };
-//          St_list.removeStudent("int22024");      
+        //cau 11
+        System.out.println("-----------------Test Same by Group----------------");
+        a.setName("Thanh");
+        a.setGroup("1123");
+        b.setName("Nam");
+        b.setGroup("1123");
+        c.setName("Sang");
+        c.setGroup("1124");
+        d.setName("Hung");
+        d.setGroup("1124");
+        e.setName("Manh");
+        e.setGroup("1125");
+        
+        Student_Management.studentsByGroup();
+        
     }
 }
